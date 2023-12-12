75338af46682afaf19c8a990771ad935532a0794
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-12 22:47:36 +0200

    commit1

--- app/src/main/java/com/example/buttons22/MainActivity.java
+++ app/src/main/java/com/example/buttons22/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.buttons22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btn(View view) {
+        counter++;
+        btn = findViewById(R.id.btn);
+        btn.setText("This is a click number:" + counter);
+        if (counter < 7){
+            btn.setText("This is a click number:" + counter);
 }
